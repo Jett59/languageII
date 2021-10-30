f6e94a565847dfee3d19359249753d23ed8b8690
Jett Thompson <jett.thompson.59@gmail.com>
2021-10-30 12:54:32 +1100

    made main.cpp decide weather to compile or interpret

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <string>
 
+using std::cerr;
 using std::cout;
 using std::endl;
+using std::string;
 
-int main () {
-  cout << "LII" << endl;
+enum class Action { COMPILE, INTERPRET };
+
+int main (int argc, char** argv) {
+  string fileName;
+  Action action = Action::INTERPRET;
+  for (int i = 1; i < argc; i++) {
+    string arg = string(argv[i]);
+    if (arg.starts_with("-")) {
+      continue;
+    }else {
+        if (arg.ends_with(".lii")) {
+          action = Action::COMPILE;
+          fileName = arg;
+        }else if (arg.ends_with(".lli")) {
+          action = Action::INTERPRET;
+          fileName = arg;
+        }
+    }
+  }
+  if (fileName == "") {
+    cerr << "Error: no input file" << endl;
+    return -1;
+  }
   return 0;
 }
